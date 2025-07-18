907da6c27c0aaa65f1fd90d50b79f494305da228
Safat11 <safat3622@gmail.com>
2025-07-18 19:33:22 +0600

    (Recap) Create a JWT Token And Save It On Client Side

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ async function run() {
         const reviewCollection = client.db("bistroDb").collection("reviews");
         const cartCollection = client.db("bistroDb").collection("carts");
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.send({ token });
+        })
+
         // users related apis
         app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
@@ -52,9 +59,9 @@ async function run() {
             res.send(result);
         })
 
-        app.patch('/users/admin/:id', async(req, res) => {
+        app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId (id)};
+            const filter = { _id: new ObjectId(id) };
             const updateDoc = {
                 $set: {
                     role: 'admin'
